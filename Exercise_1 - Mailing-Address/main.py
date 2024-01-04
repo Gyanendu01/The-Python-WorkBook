e4c9066fecb106007ef22e09d880f4023fe90b28
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2024-01-04 22:31:24 +0530

    Add exercise 1 along with main.py

---
+++ Exercise_1 - Mailing-Address/main.py
@@ -0,0 +1,3 @@
+print("\n\tMy name is: Gyanendu Bikash Boitai")
+print("\n\tMAILING ADDRESS")
+print("\tStreet:  222ndmnrdvylkvlblr-3, Main Road\n\tCity:\tBangalore\n\tState/province/area: Karnataka\n\tZip code: 560003\n\tCountry calling code: +91\n\tCountry: India")
