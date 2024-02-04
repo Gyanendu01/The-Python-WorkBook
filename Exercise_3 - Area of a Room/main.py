e8abf8e9b575ce97520ec8c46621e9a94e94e8e5
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2024-02-04 09:15:52 +0530

    Add exercise 3

---
+++ Exercise_3 - Area of a Room/main.py
@@ -0,0 +1,4 @@
+roomWidth = float(input("Enter the width of the room (in meters): "))
+roomLength = float(input("Entr the length of the room (in meters): "))
+roomArea = roomWidth * roomLength
+print("\nThe Area of the room is {} m**2".format(roomArea))
