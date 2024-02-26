f790cfc7d27deeee89abc35256d80a825893c109
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2024-02-26 20:25:20 +0530

    Add Exercise_10

---
+++ Exercise_10-Arithmetic/main.py
@@ -0,0 +1,10 @@
+import math
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+print(f"\nThe sum of {a} and {b} is {a+b}")
+print(f"The difference when {b} is subtracted from {a} is {a-b}")
+print(f"The product of {a} and {b} is {a*b}")
+print(f"The quotient when {a} is divided by {b} is {a/b}")
+print(f"The remainder when {a} is divided by {b} is {a%b}")
+print(f"The result of log10{a} is {math.log10(a)}")
+print(f"The result of {a}^{b} is {a**b}")
